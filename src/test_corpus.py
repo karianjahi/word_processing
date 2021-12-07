588f88d65a6ac7a959a06f070912019a0d92bbad
Joseph Njeri <njeri@edi.gmbh>
2021-12-07 12:06:48 +0100

    Add docstring to test_corpus

--- src/test_corpus.py
+++ src/test_corpus.py
@@ -1,4 +1,9 @@
 from word_processor import count_words
 
 def test_corpus(get_text_from_file):
+    """
+    testing a fixture
+    :param get_text_from_file: name of the function
+    :return: None
+    """
     assert count_words(get_text_from_file) > 10
